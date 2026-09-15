1eaa9295f5d9993adbc32f7fcb9b4abf0d3494ef
vipulgit-123 <vipul99819@gmail.com>
2026-09-15 23:29:57 +0530

    backed

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,10 @@ const express = require('express');
 const app = express()
 const port = 3000
 
+// Available routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 app.get('/', (req, res) => {
   res.send('Hello World... I am Vipul!')
 })
