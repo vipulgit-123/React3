1816c810792e0f9727d1cbc34718a9c1ee96f3a4
vipulgit-123 <vipul99819@gmail.com>
2026-09-20 23:53:15 +0530

    Login endpoint and sending AuthToken

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -31,28 +31,21 @@ router.post('/createuser',[
             if (user) {
                 return res.status(400).json({errors: "Sorry a user with this email already exists"})
             }
-
             const salt = await bcrypt.genSalt(10);
             const secPass = await bcrypt.hash(req.body.password,salt)
-
             user = await User.create({
                 name: req.body.name,
                 email: req.body.email,
                 password: secPass,
             })
-
-
             const data ={
                 user:
                     {
                         id: user.id
                     }
             }
-
             const authtoken = jwt.sign(data, JWT_Secret)
             console.log(authtoken)
-
-            // res.json({user})
             res.json({authtoken})
 
         }catch (e) {
@@ -60,4 +53,39 @@ router.post('/createuser',[
             res.status(500).send("Some error occured")
         }
     })
+
+//-----------------------------------------------------------------------------------------------------------
+// Authenticate a User using: POST "/api/auth/login". NO login required
+router.post('/login',[
+     body('email','Enter a valid email').isEmail(),
+    body('password','Password can`t be blank').exists(),
+], async (req, res) => {
+
+    const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
+
+        const {email,password} = req.body;
+        try{
+            let user = await User.findOne({email})
+            if (!user){return  res.status(400).json({error:"Please try to login via right credentials"})}
+
+            const passCompare = await bcrypt.compare(password,user.password)
+            if (!passCompare){return res.status(400).json({error:"Please try to login via right credentials"})}
+
+            const data ={
+                  user:
+                    {
+                        id: user.id
+                    }
+            }
+            const authtoken = jwt.sign(data, JWT_Secret)
+
+            res.json({authtoken})
+        }catch (e) {
+            console.log(e.message)
+            res.status(500).send("Internal Server occured")
+        }
+})
 module.exports = router
