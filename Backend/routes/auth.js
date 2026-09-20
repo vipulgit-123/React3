db0c8a882c861bd5bce07a00d622e434b4058f0e
vipulgit-123 <vipul99819@gmail.com>
2026-09-20 23:30:49 +0530

    JWT Token

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -5,7 +5,9 @@ const User = require('../models/User')
 const router = express.Router()
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
 
+const JWT_Secret = '#@IamABaDBoy#BAD_BOY'
 //Create a user using: POST "/api/auth/". Doesn't require Auth
 router.post('/createuser',[
     body('name','Enter a valid name').isLength({min:3}),
@@ -38,7 +40,21 @@ router.post('/createuser',[
                 email: req.body.email,
                 password: secPass,
             })
-            res.json({user})
+
+
+            const data ={
+                user:
+                    {
+                        id: user.id
+                    }
+            }
+
+            const authtoken = jwt.sign(data, JWT_Secret)
+            console.log(authtoken)
+
+            // res.json({user})
+            res.json({authtoken})
+
         }catch (e) {
             console.log(e.message)
             res.status(500).send("Some error occured")
