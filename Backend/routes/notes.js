7ed67cd9b2fc02d6bed56c83aeb3bb63f9112400
vipulgit-123 <vipul99819@gmail.com>
2026-09-21 23:48:56 +0530

    fetchallNotes of the user

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require("mongoose");
+var fetchuser  = require('../middleware/fetchuser')
+const Notes = require('../models/Notes');
 
+// ----------------------------------------------------------------------------------------------------------
+//Route 1 Get all the notes: Get "/api/notes/fetchAllNotes". to login required
+router.get('/fetchAllNotes', fetchuser, async (req, res) => {
+    const notes = await Notes.find({user: req.user.id})
+    res.json(notes)
 
-router.get('/',(req,res)=>{
-
-    res.json([])
 })
 module.exports = router
