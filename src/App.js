d07183baec658d1faeadb7aec2240077b35c4c39
vipulgit-123 <vipul99819@gmail.com>
2026-09-22 23:49:48 +0530

    frontend Initials

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Home from "./components/Home";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Router>
+         <Navbar/>
+  <Routes>
+    <Route exact path="/home" element={<Home />} />
+    <Route exact path="/about" element={<About />} />
+  </Routes>
+</Router>
     </div>
   );
 }
