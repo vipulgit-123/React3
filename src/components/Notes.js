d2a91ce2cbc35303e219b9b33e22c8805d96e615
vipulgit-123 <vipul99819@gmail.com>
2026-09-25 18:20:54 +0530

    Adding AddNote to the Component

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,18 +1,22 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import noteContext from "../context/Notes/noteContext";
 import Noteitem from "./Noteitem";
+import AddNotes from "./AddNotes";
 
 const Notes = () => {
-    const context = useContext(noteContext);
-    const {notes, setNotes} = context;
-          return (
-              <div className="row my-3">
-                  <h2> You Note</h2>
-      {notes.map((notes)=>{
-          return <Noteitem key={notes._id} notes={notes}/>
+  const context = useContext(noteContext);
+  const { notes, addNotes } = context;
+  return (
+    <>
+      <AddNotes />
+      <div className="row my-3">
+        <h2> You Note</h2>
+        {notes.map((notes) => {
+          return <Noteitem key={notes._id} notes={notes} />;
         })}
-  </div>
-    )
+      </div>
+    </>
+  );
 };
 
 export default Notes;
--- src/context/Notes/NoteState.js
+++ src/context/Notes/NoteState.js
@@ -23,7 +23,7 @@ const NoteState = (props) => {
         "__v": 0
     },
         {
-        "_id": "6ab50db9f8fa9fd1328e555c",
+        "_id": "6ab50db9f8fa9fd1328e5255c",
         "user": "6ab505fdf8fa9fd1328e555a",
         "title": "Note 1",
         "description": "This is my story1",
@@ -32,7 +32,7 @@ const NoteState = (props) => {
         "__v": 0
     },
     {
-        "_id": "6ab50e55cd81fd03f7363317",
+        "_id": "6ab50e55cd81fd03f73633317",
         "user": "6ab505fdf8fa9fd1328e555a",
         "title": "Note 2",
         "description": "This is my story2",
@@ -41,7 +41,7 @@ const NoteState = (props) => {
         "__v": 0
     },
       {
-        "_id": "6ab50db9f8fa9fd1328e555c",
+        "_id": "6ab50db9f8fa9fd13238e555c",
         "user": "6ab505fdf8fa9fd1328e555a",
         "title": "Note 1",
         "description": "This is my story1",
@@ -50,7 +50,41 @@ const NoteState = (props) => {
         "__v": 0
     },
      {
-        "_id": "6ab50e55cd81fd03f7363317",
+        "_id": "6ab50e55cd81fd03f73633117",
+        "user": "6ab505fdf8fa9fd1328e555a",
+        "title": "Note 2",
+        "description": "This is my story2",
+        "tag": "2",
+        "date": "1790250581402",
+        "__v": 0
+    }, {
+        "_id": "6ab50db9f8fa9fd1328e2555c",
+        "user": "6ab505fdf8fa9fd1328e555a",
+        "title": "Note 1",
+        "description": "This is my story1",
+        "tag": "1",
+        "date": "1790250425283",
+        "__v": 0
+    },
+     {
+        "_id": "6ab50e55cd81fd03f73613317",
+        "user": "6ab505fdf8fa9fd1328e555a",
+        "title": "Note 2",
+        "description": "This is my story2",
+        "tag": "2",
+        "date": "1790250581402",
+        "__v": 0
+    }, {
+        "_id": "6ab50db9f8fa9fd13284e555c",
+        "user": "6ab505fdf8fa9fd1328e555a",
+        "title": "Note 1",
+        "description": "This is my story1",
+        "tag": "1",
+        "date": "1790250425283",
+        "__v": 0
+    },
+     {
+        "_id": "6ab50e55cd81fd03f73563317",
         "user": "6ab505fdf8fa9fd1328e555a",
         "title": "Note 2",
         "description": "This is my story2",
@@ -62,8 +96,35 @@ const NoteState = (props) => {
 
     const [notes, setNotes] = useState(notesInitial)
 
+    //Add a Note
+    const addNotes =(title,description,tag)=>{
+        // ToDo: Api calling
+
+        console.log("Adding a new note")
+        let note={
+        "_id": "6ab50e55cd81fd03f7363317",
+        "user": "6ab505fdf8fa9fd1328e555a",
+        "title": title,
+        "description": description,
+        "tag": tag,
+        "date": "1790250581402",
+        "__v": 0
+    }
+        setNotes(notes.concat(note))
+    }
+
+    //Dlt a Note
+    const dltNotes =()=>{
+
+    }
+
+    //Edit a Note
+    const edtNotes =()=>{
+
+    }
+
     return(
-        <NoteContext.Provider value={{notes,setNotes}}>
+        <NoteContext.Provider value={{notes,addNotes, dltNotes, edtNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
