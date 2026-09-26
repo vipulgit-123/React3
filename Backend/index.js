8b553cac207d594ae49b2176f38682e7d52685c9
vipulgit-123 <vipul99819@gmail.com>
2026-09-26 12:32:36 +0530

    fixing Cors error

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,14 @@
 const connectToMongo = require('./db')
+var cors = require('cors');
 
 connectToMongo()
 const express = require('express');
 const app = express()
-const port = 5000
+const port = 3000
+
+app.use(cors());
+app .use(express.json());
+
 
 //middleware
 app.use(express.json()) //if u want to use req.body then we have to use this middleware
