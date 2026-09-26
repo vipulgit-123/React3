052b57d7fc8d80385a80b08f38706cfe5da1325c
vipulgit-123 <vipul99819@gmail.com>
2026-09-26 23:20:24 +0530

    Adding Login/SignUp component in iNoteBook

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from "./components/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NoteState from "./context/Notes/NoteState";
 import Alert from "./components/Alert";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
                 <Route path="/" element={<Home />} />
               <Route exact path="/home" element={<Home />} />
               <Route exact path="/about" element={<About />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/signup" element={<SignUp />} />
             </Routes>
           </div>
         </Router>
